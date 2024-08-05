f60a6cc083ebe62f7daaa979955669d0718dea1f
Hugo Salinas <anguyapy@gmail.com>
2024-08-05 12:08:16 -0400

    Actualizacion de Sucursales

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,9 @@
 <select v-model="selected">
         <option disabled value="">Seleccione la Sucursal</option>
         <option>Casa Central</option>
+        <option>Farmacia</option>
+        <option>Clinica Médica</option>
+        <option>Biblioteca</option>
         <option>Agropecuario</option>
-        <option>Hohenau</option>
+        <option>Hohenau</option>        
 </select>
